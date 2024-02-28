5e4c05286275eff5f4f8d007c611f71743e9a87c
Shawn <sljt@proton.me>
2024-02-28 10:23:59 -0500

    complete scanner exercises

--- mooc-java-programming-i/part01-Part01_09.Conversation/src/main/java/Conversation.java
+++ mooc-java-programming-i/part01-Part01_09.Conversation/src/main/java/Conversation.java
@@ -7,6 +7,10 @@ public class Conversation {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        System.out.println("Greetings! How are you doing?");
+        scanner.nextLine();
+        System.out.println("Oh, how interesting. Tell me more!");
+        scanner.nextLine();
+        System.out.println("Thanks for sharing!");      
     }
 }
--- mooc-java-programming-i/part01-Part01_10.Story/src/main/java/Story.java
+++ mooc-java-programming-i/part01-Part01_10.Story/src/main/java/Story.java
@@ -7,6 +7,15 @@ public class Story {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        System.out.println("I will tell you a story, but I need some information first.");
+        System.out.println("What is the main character called?");
+        String firstInput = scanner.nextLine();
+        System.out.println("What is their job?");
+        String secondInput = scanner.nextLine();
+        System.out.println("Here is the story:");
+        System.out.println("Once upon a time there was " + firstInput + ", who was " + secondInput + ".");
+        System.out.println("On the way to work, " + firstInput + " reflected on life.");
+        System.out.println("Perhaps " + firstInput + " will not be " + secondInput + " forever.");
+        
     }
 }
